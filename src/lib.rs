ad5e77959ddfc4d2720cb561c6d1e274ff3fb77a
Julius Liu <juliusl@microsoft.com>
2022-06-05 21:36:55 -0700

    Feat. stubbing out refactored types

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,177 @@
-use atlier::system::Attribute;
+use atlier::system::{Attribute, Value};
 use logos::Logos;
 use parser::Lifecycle;
+use serde::{Serialize, Deserialize};
+use specs::Entity;
 use std::any::Any;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::{Debug, Display};
 
 pub mod editor;
 pub mod plugins;
+
+#[derive(Debug, Default, Clone, Hash, Serialize, Deserialize, PartialEq, PartialOrd)]
+pub struct AttributeGraph {
+    entity: u32,
+    index: BTreeSet<Attribute>
+}
+
+impl From<Entity> for AttributeGraph {
+    fn from(entity: Entity) -> Self {
+        AttributeGraph { entity: entity.id(), index: BTreeSet::default() }
+    }
+}
+
+impl AttributeGraph {
+    pub fn with(&mut self, name: impl AsRef<str>, value: Value) -> Self {
+        self.update(move |g| match value {
+            Value::Empty => {
+                g.add_empty_attr(name);
+            },
+            Value::Symbol(symbol) => {
+                g.add_symbol(name, symbol);
+            },
+            Value::TextBuffer(text_buffer) => {
+                g.add_text_attr(name, text_buffer);
+            },
+            Value::Float(init_value) => {
+                g.add_float_attr(name, init_value);
+            },
+            Value::Int(init_value) => {
+                g.add_int_attr(name, init_value);
+            },
+            Value::Bool(init_value) => {
+                g.add_bool_attr(name, init_value);
+            },
+            Value::IntPair(e0, e1) => {
+                g.add_int_pair_attr(name, &[e0, e1]);
+            },
+            Value::FloatPair(e0, e1) => {
+                g.add_float_pair_attr(name, &[e0, e1]);
+            },
+            Value::FloatRange(value, min, max) => {
+                g.add_float_range_attr(name, &[value, min, max]);
+            },
+            Value::IntRange(value, min, max) => {
+                g.add_int_range_attr(name, &[value, min, max]);
+            },
+            Value::BinaryVector(init_value) => {
+                g.add_binary_attr(name, init_value);
+            },
+            Value::Reference(init_value) => {
+                g.add_reference(name, init_value);
+            },
+        })
+    }
+
+    pub fn add_reference(&mut self, name: impl AsRef<str>, init_value: impl Into<u64>) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::Reference(init_value.into()),
+        ));
+    }
+
+    pub fn add_symbol(&mut self, name: impl AsRef<str>, symbol: impl AsRef<str>) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::Symbol(symbol.as_ref().to_string()),
+        ));
+    }
+
+    pub fn add_empty_attr(&mut self, name: impl AsRef<str>) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::Empty,
+        ));
+    }
+
+    pub fn add_binary_attr(&mut self, name: impl AsRef<str>, init_value: impl Into<Vec<u8>>) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::BinaryVector(init_value.into()),
+        ));
+    }
+
+    pub fn add_text_attr(&mut self, name: impl AsRef<str>, init_value: impl AsRef<str>) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::TextBuffer(init_value.as_ref().to_string()),
+        ));
+    }
+
+    pub fn add_int_attr(&mut self, name: impl AsRef<str>, init_value: i32) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::Int(init_value),
+        ));
+    }
+
+    pub fn add_float_attr(&mut self, name: impl AsRef<str>, init_value: f32) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::Float(init_value),
+        ));
+    }
+
+    pub fn add_bool_attr(&mut self, name: impl AsRef<str>, init_value: bool) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::Bool(init_value),
+        ));
+    }
+
+    pub fn add_float_pair_attr(&mut self, name: impl AsRef<str>, init_value: &[f32; 2]) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::FloatPair(init_value[0], init_value[1]),
+        ));
+    }
+
+    pub fn add_int_pair_attr(&mut self, name: impl AsRef<str>, init_value: &[i32; 2]) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::IntPair(init_value[0], init_value[1]),
+        ));
+    }
+
+    pub fn add_int_range_attr(&mut self, name: impl AsRef<str>, init_value: &[i32; 3]) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::IntRange(init_value[0], init_value[1], init_value[2]),
+        ));
+    }
+
+    pub fn add_float_range_attr(&mut self, name: impl AsRef<str>, init_value: &[f32; 3]) {
+        self.add_attribute(Attribute::new(
+            self.entity,
+            name.as_ref().to_string(),
+            Value::FloatRange(init_value[0], init_value[1], init_value[2]),
+        ));
+    }
+
+    pub fn add_attribute(&mut self, attr: Attribute) {
+        self.index.insert(attr);
+    }
+
+    pub fn update(&mut self, func: impl FnOnce(&mut Self)) -> Self {
+        let next = self;
+
+        (func)(next);
+
+        next.to_owned()
+    }
+}
 
 pub trait RuntimeState: Any + Sized + Clone + Sync + Default + Send + Display {
     type Error;
---
+++ src/plugins/render/mod.rs
@@ -0,0 +1,93 @@
+use std::collections::{BTreeMap, HashMap};
+
+use specs::{Component, System, Entities, WriteStorage, ReadStorage, Join};
+use specs::storage::DenseVecStorage;
+
+use crate::AttributeGraph;
+
+mod imgui {
+    pub use imgui::Ui;
+    use specs::Component;
+    use specs::storage::DenseVecStorage;
+
+    use crate::AttributeGraph;
+
+    pub type RenderFn = fn(&mut AttributeGraph, &Ui);
+
+    #[derive(Clone, Component)]
+    #[storage(DenseVecStorage)]
+    pub struct RenderComponent(pub RenderFn);
+}
+pub use self::imgui::RenderFn;
+pub use self::imgui::RenderComponent;
+
+/// Render is a struct wrapper over a render function for imgui
+#[derive(Clone, Component)]
+#[storage(DenseVecStorage)]
+pub enum Render{
+        Empty,
+        Func(RenderFn),
+        Output(Option<AttributeGraph>),
+}
+
+impl Default for Render
+{
+    fn default() -> Self {
+        Self::Empty
+    }
+}
+
+pub struct RenderSystem 
+{    
+    funcs: BTreeMap<u32, Render>,
+    output: HashMap<u32, AttributeGraph>
+}
+
+impl<'a> System<'a> for RenderSystem 
+{
+    type SystemData = (
+        Entities<'a>,
+        ReadStorage<'a, RenderComponent>,
+        WriteStorage<'a, Render>,
+    );
+
+    fn run(&mut self, (entities, render_component, mut render_output): Self::SystemData) {
+        for (e, RenderComponent(render)) in (&entities, &render_component).join() {
+            if let None = self.funcs.get(&e.id()) {
+                self.funcs.insert(e.id(), Render::Func(render.clone()));
+            }
+        }
+
+        for (key, next) in self.output.iter() {
+            let entity = entities.entity(*key); 
+            if entities.is_alive(entity) {
+                match render_output.get(entity) {
+                    None | Some(Render::Empty) | Some(Render::Output(None))  => {
+                        match render_output.insert(entity, Render::Output(Some(next.clone()))) {
+                            Ok(_) => todo!(),
+                            Err(_) => todo!(),
+                        }
+                    }
+                    Some(Render::Output(Some(current))) => {
+                        if next != current {
+                            render_output.remove(entity);
+                        }
+                    }
+                    _ => (),
+                }
+            } else {
+                let to_remove = &entity.id();
+                self.funcs.remove(to_remove);
+            }
+        }
+
+        for (key, next) in self.output.iter_mut() {
+            let entity = entities.entity(*key);
+            if !entities.is_alive(entity) {
+               
+            }
+        }
+
+        todo!()
+    }
+}
