5dc3e4d3e1b665e103cb52387a80ebb9168bb69f
Julius Liu <juliusl@microsoft.com>
2022-06-29 20:54:53 -0700

    Feat. record metadat for remote Feat. get command from context

--- src/plugins/process/mod.rs
+++ src/plugins/process/mod.rs
@@ -23,49 +23,50 @@ impl Plugin<ThunkContext> for Process {
         context.clone().task(|| {
             let mut tc = context.clone();
             async move {
-                if let Some(command) = tc.as_ref().find_text("command") {
-                    // Creating a new tokio command
-                    let parts: Vec<&str> = command.split(" ").collect();
-                    tc.update_progress(format!("``` {} process", tc.block.block_name), 0.10)
+                let command = tc
+                    .as_ref()
+                    .find_text("command")
+                    .unwrap_or("echo missing command".to_string());
+                // Creating a new tokio command
+                let parts: Vec<&str> = command.split(" ").collect();
+                tc.update_progress(format!("``` {} process", tc.block.block_name), 0.10)
+                    .await;
+                if let Some(command) = parts.get(0) {
+                    tc.update_progress(format!("add command .text {}", command), 0.10)
+                        .await;
+                    let mut command_task = tokio::process::Command::new(&command);
+                    for (el, arg) in parts.iter().skip(1).enumerate() {
+                        command_task.arg(arg);
+                        tc.update_progress(format!("add arg{}    .text {}", el, arg), 0.10)
                             .await;
-                    if let Some(command) = parts.get(0) {
-                        tc.update_progress(format!("add command .text {}", command), 0.10)
-                            .await;
-                        let mut command_task = tokio::process::Command::new(&command);
-                        for (el, arg) in parts.iter().skip(1).enumerate() {
-                            command_task.arg(arg);
-                            tc.update_progress(format!("add arg{}    .text {}", el, arg), 0.10)
+                    }
+                    tc.update_progress("```", 0.10).await;
+                    tc.update_progress("# Running", 0.20).await;
+                    let start_time = Some(Utc::now());
+                    match command_task.output().await {
+                        Ok(output) => {
+                            // Completed process, publish result
+                            tc.update_progress("# Finished, recording output", 0.30)
                                 .await;
+                            let timestamp_utc = Some(Utc::now().to_string());
+                            let timestamp_local = Some(Local::now().to_string());
+                            let elapsed = start_time
+                                .and_then(|s| Some(Utc::now() - s))
+                                .and_then(|d| Some(format!("{} ms", d.num_milliseconds())));
+                            tc.as_mut()
+                                .with_int("code", output.status.code().unwrap_or_default())
+                                .with_binary("stdout", output.stdout)
+                                .with_binary("stderr", output.stderr)
+                                .with_text("timestamp_local", timestamp_local.unwrap_or_default())
+                                .with_text("timestamp_utc", timestamp_utc.unwrap_or_default())
+                                .with_text("elapsed", elapsed.unwrap_or_default());
                         }
-                        tc.update_progress("```", 0.10).await;
-                        tc.update_progress("# Running", 0.20).await;
-                        let start_time = Some(Utc::now());
-                        match command_task.output().await {
-                            Ok(output) => {
-                                // Completed process, publish result
-                                tc.update_progress("# Finished, recording output", 0.30).await;
-                                let timestamp_utc = Some(Utc::now().to_string());
-                                let timestamp_local = Some(Local::now().to_string());
-                                let elapsed = start_time
-                                    .and_then(|s| Some(Utc::now() - s))
-                                    .and_then(|d| Some(format!("{} ms", d.num_milliseconds())));
-                                tc.as_mut()
-                                    .with_int("code", output.status.code().unwrap_or_default())
-                                    .with_binary("stdout", output.stdout)
-                                    .with_binary("stderr", output.stderr)
-                                    .with_text(
-                                        "timestamp_local",
-                                        timestamp_local.unwrap_or_default(),
-                                    )
-                                    .with_text("timestamp_utc", timestamp_utc.unwrap_or_default())
-                                    .with_text("elapsed", elapsed.unwrap_or_default());
-                            }
-                            Err(err) => {
-                                tc.update_progress(format!("# error {}", err), 0.0).await;
-                            }
+                        Err(err) => {
+                            tc.update_progress(format!("# error {}", err), 0.0).await;
                         }
                     }
                 }
+
                 Some(tc)
             }
         })
--- src/plugins/process/remote.rs
+++ src/plugins/process/remote.rs
@@ -1,4 +1,5 @@
 use crate::plugins::{Plugin, ThunkContext};
+use chrono::{Local, Utc};
 use specs::storage::DenseVecStorage;
 use specs::Component;
 use std::process::Stdio;
@@ -19,24 +20,57 @@ impl Plugin<ThunkContext> for Remote {
     ) -> Option<tokio::task::JoinHandle<ThunkContext>> {
         context.clone().task(|| {
             let log = context.clone();
-            let tc = context.clone();
+            let mut tc = context.clone();
             let child_handle = context.handle().clone();
             async move {
-                // TODO: put actual command here
-                let mut cmd = Command::new("zsh");
-                cmd.stdout(Stdio::piped());
+                let cmd = tc.as_ref()
+                    .find_text("command")
+                    .unwrap_or("echo missing command".to_string());
+                let parts: Vec<&str> = cmd.split(" ").collect();
+                tc.update_progress(format!("``` {} process", tc.block.block_name), 0.10)
+                            .await;
+                if let Some(command) = parts.get(0) {
+                    tc.update_progress(format!("add command .text {}", command), 0.10).await;
+                    let mut command_task = Command::new(&command);
+                    for (el, arg) in parts.iter().skip(1).enumerate() {
+                            command_task.arg(arg);
+                            tc.update_progress(format!("add arg{}    .text {}", el, arg), 0.10)
+                                .await;
+                    }
+                    tc.update_progress("```", 0.10).await;
+                    command_task.stdout(Stdio::piped());
 
-                if let Some(mut child) = cmd.spawn().ok() {
-                    if let Some(stdout) = child.stdout.take() {
-                        let mut reader = BufReader::new(stdout).lines();
+                    if let Some(mut child) = command_task.spawn().ok() {
+                        if let Some(stdout) = child.stdout.take() {
+                            let mut reader = BufReader::new(stdout).lines();
 
-                        if let Some(handle) = child_handle {
-                            let _child_task = handle.spawn(async move {
-                                tc.update_status_only("# child process started").await;
-                                if let Some(output) = child.wait_with_output().await.ok() {
-                                    eprintln!("exiting child process {:?}", output.status.code());
+                            if let Some(handle) = child_handle {
+                                let _child_task = handle.spawn(async move {
+                                tc.update_progress("# child process started, stdout/stdin are being piped to console", 0.50).await;
+                                let start_time = Some(Utc::now());
+                                match child.wait_with_output().await {
+                                    Ok(output) => {
+                                        // Completed process, publish result
+                                        tc.update_progress("# child is exiting, recording output", 0.80).await;
+                                        let timestamp_utc = Some(Utc::now().to_string());
+                                        let timestamp_local = Some(Local::now().to_string());
+                                        let elapsed = start_time
+                                            .and_then(|s| Some(Utc::now() - s))
+                                            .and_then(|d| Some(format!("{} ms", d.num_milliseconds())));
+                                        tc.as_mut()
+                                            .with_int("code", output.status.code().unwrap_or_default())
+                                            .with_binary("stderr", output.stderr)
+                                            .with_text(
+                                                "timestamp_local",
+                                                timestamp_local.unwrap_or_default(),
+                                            )
+                                            .with_text("timestamp_utc", timestamp_utc.unwrap_or_default())
+                                            .with_text("elapsed", elapsed.unwrap_or_default());
+                                    }
+                                    Err(err) => {
+                                        tc.update_progress(format!("# error {}", err), 0.0).await;
+                                    }
                                 }
-                                // TODO: record output
                                 tc
                             });
 
@@ -44,7 +78,8 @@ impl Plugin<ThunkContext> for Remote {
                             loop {
                                 match reader.next_line().await {
                                     Ok(Some(line)) => {
-                                        log.update_status_only(format!("{}", line)).await;
+                                        eprintln!("{}", line);
+                                        log.update_status_only(line).await;
                                     }
                                     Err(err) => {
                                         eprintln!("err: {}", err);
@@ -55,11 +90,12 @@ impl Plugin<ThunkContext> for Remote {
                                     }
                                 }
                             }
-
-                            return _child_task.await.ok()
+                                return _child_task.await.ok()
+                            }
                         }
                     }
                 }
+
                 log.update_status_only("Could not spawn child process").await;
                 None
             }
