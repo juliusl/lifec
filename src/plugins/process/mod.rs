facdddddd9652001a7767445cf59fa63cf7edfcb
Julius Liu <juliusl@microsoft.com>
2022-07-24 23:31:51 -0700

    Write stdout to char device

--- src/plugins/process/mod.rs
+++ src/plugins/process/mod.rs
@@ -160,6 +160,9 @@ impl Plugin<ThunkContext> for Process {
                        output = command_task.output() => {
                             match output {
                                 Ok(output) => {
+                                    for b in output.stdout.clone() {
+                                        tc.send_char(b).await;
+                                    }
                                     // Completed process, publish result
                                     tc.update_progress("# Finished, recording output", 0.30).await;
                                     Self::resolve_output(&mut tc, command, start_time, output);
