a10814cdb1abe1d9d7d0ba1d4d12f02c35c137ce
Julius Liu <juliusl@microsoft.com>
2022-07-26 18:48:03 -0700

    feat. add work_dir attr handling to redirect

--- src/plugins/process/redirect.rs
+++ src/plugins/process/redirect.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use crate::plugins::{ThunkContext, Plugin};
 
 #[derive(Default)]
@@ -19,8 +21,17 @@ impl Plugin<ThunkContext> for Redirect {
                 tc.as_mut().apply("previous");
 
                 for process_block in tc.as_ref().find_blocks("process") {
-                    if let Some(redirect_stdout) = tc.as_ref().find_text("redirect_stdout") {
+                    if let Some(mut redirect_stdout) = tc.as_ref().find_text("redirect_stdout") {
                         if let Some(stdout) = process_block.find_binary("stdout") {
+
+                            if let Some(work_dir) = tc.as_ref().find_text("work_dir") {
+                                let work_dir = PathBuf::from(work_dir);
+                                if work_dir.exists() {
+                                    let redirect = work_dir.join(&redirect_stdout);
+                                    redirect_stdout = redirect.to_str().unwrap_or_default().to_string();
+                                }
+                            }
+
                             match tokio::fs::write(redirect_stdout, stdout).await {
                                 Ok(_) => {
                                     
@@ -32,8 +43,17 @@ impl Plugin<ThunkContext> for Redirect {
                         }
                     }
 
-                    if let Some(redirect_stderr) =  tc.as_ref().find_text("redirect_stderr") {
+                    if let Some(mut redirect_stderr) =  tc.as_ref().find_text("redirect_stderr") {
                         if let Some(stderr) = process_block.find_binary("stderr") {
+                            
+                            if let Some(work_dir) = tc.as_ref().find_text("work_dir") {
+                                let work_dir = PathBuf::from(work_dir);
+                                if work_dir.exists() {
+                                    let redirect = work_dir.join(&redirect_stderr);
+                                    redirect_stderr = redirect.to_str().unwrap_or_default().to_string();
+                                }
+                            }
+
                             match tokio::fs::write(redirect_stderr, stderr).await {
                                 Ok(_) => {
                                     
