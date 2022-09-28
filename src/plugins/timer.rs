9a60331ac48e5aaac3c8e4dd44b41686bdbd440f
Julius Liu <juliusl@microsoft.com>
2022-09-28 11:28:08 -0700

    Fix. timer plugin

--- src/plugins/timer.rs
+++ src/plugins/timer.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
-use crate::{BlockObject, BlockProperties};
 use crate::plugins::*;
+use crate::{BlockObject, BlockProperties};
 use logos::{Lexer, Logos};
 use specs::storage::DenseVecStorage;
 use tokio::task::JoinHandle;
@@ -25,15 +25,17 @@ impl Plugin for Timer {
 
     fn call(thunk_context: &ThunkContext) -> Option<(JoinHandle<ThunkContext>, CancelToken)> {
         thunk_context.clone().task(|mut cancel_source| {
-            let tc = thunk_context.clone();
+            let mut tc = thunk_context.clone();
             async move {
                 // Parse the input to timer
-                let duration = tc.state().find_symbol("timer").and_then(|d| {
-                    match TimerSettings::lexer(&d).next() {
+                let duration = tc
+                    .state()
+                    .find_symbol("timer")
+                    .and_then(|d| match TimerSettings::lexer(&d).next() {
                         Some(TimerSettings::Duration(duration)) => Some(duration),
                         _ => Some(0.0),
-                    }
-                }).unwrap_or_default();
+                    })
+                    .unwrap_or_default();
 
                 // let duration = Duration::from_secs_f32(duration);
                 // tokio::time::sleep(duration).await;
@@ -53,7 +55,8 @@ impl Plugin for Timer {
                             .await;
                         }
                     } else {
-                        // tc.add_text_attr("elapsed", format!("{:?}", elapsed));
+                        tc.state_mut()
+                            .with_symbol("elapsed", format!("{:?}", elapsed));
                         break;
                     }
 
@@ -62,8 +65,7 @@ impl Plugin for Timer {
                     }
                 }
 
-                // There are no updates
-                None
+                Some(tc)
             }
         })
     }
@@ -119,5 +121,5 @@ fn on_duration(lexer: &mut Lexer<TimerSettings>) -> Option<f32> {
 #[test]
 fn test_timer_settings() {
     let mut lexer = TimerSettings::lexer("100 ms");
-    assert_eq!(lexer.next(), Some(TimerSettings::Duration(100.0/1000.0)));
+    assert_eq!(lexer.next(), Some(TimerSettings::Duration(100.0 / 1000.0)));
 }
